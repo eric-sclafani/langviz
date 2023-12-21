4586aa5a7de27762997b6618e38aaf2856af30f8
eric-sclafani <eric.sclafani321@gmail.com>
2023-12-21 12:49:48 -0500

    moved spacy to own module, implemented spacy extensions, cleaned doc processing code

--- src/langviz/processing/__init__.py
+++ src/langviz/processing/__init__.py
@@ -3,75 +3,73 @@ This module contains text processing
 """
 
 from dataclasses import dataclass
-from typing import List, Set
+from typing import Iterator, List, Set
 
-import spacy
-from spacy.cli.download import download
-from spacy.language import Language
+import numpy as np
+import pandas as pd
 from spacy.tokens import Doc, Span
 
+from langviz.processing.load_spacy import load_spacy_model
 from langviz.utils import timer
 
 
 @dataclass
 class Document:
-    """Contains attributes to be used in visualization components"""
-
     doc_id: str
-    tokens: List[str]
+    text: str
+    vector: np.ndarray
+    tokens: Iterator[str]
     types: Set[str]
-    sentences: List[Span]
-    doc: Doc
+    sentences: Iterator[Span]
+    named_entities: pd.DataFrame
 
     def __repr__(self):
-        return self.doc.text
+        return self.text
+
+    @property
+    def token_count(self) -> int:
+        return len(list(self.tokens))
+
+    @property
+    def type_count(self) -> int:
+        return len(self.types)
+
+    @property
+    def sentence_count(self) -> int:
+        return len(list(self.sentences))
+
+    @classmethod
+    def from_spacy_document(cls, doc: Doc, doc_id: str):
+        text = doc.text
+        vector = np.array(doc.vector)
+        tokens = doc._.tokens
+        types = doc._.types
+        sentences = doc.sents
+        named_entities = doc._.entities_df
+        return cls(doc_id, text, vector, tokens, types, sentences, named_entities)
+
+
+@dataclass
+class Corpus:
+    _documents: List[Document]
+
+    @property
+    def documents(self):
+        return self._documents
 
 
 # TODO: break into smaller functions
-
-
 @timer
 def process_documents(
     data: List[str], doc_ids: List[str], n_process: int
 ) -> List[Document]:
     """Converts each text document into Document object containing useful information"""
 
-    @timer
-    def load_spacy_model(model: str) -> Language:
-        """Attempts to load given spaCy model. Attempts to download if not found"""
-        try:
-            return spacy.load(model)
-        except OSError:
-            print(f"spaCy model '{model}' not detected. Downloading...")
-            download(model)
-            return spacy.load(model)
-
-    def get_token_strings(spacy_doc: Doc) -> List[str]:
-        """Returns a list of all token strings"""
-        return [token.text for token in spacy_doc]
-
-    def get_sentences(spacy_doc: Doc) -> List[Span]:
-        """Returns a list of all sentence objects"""
-        return list(spacy_doc.sents)
-
-    # TODO: experiment with medium and large model time
     nlp = load_spacy_model("en_core_web_lg")
-    nlp.max_length = 10000000
     docs = nlp.pipe(data, n_process=n_process)
 
     all_documents = []
     for doc, doc_id in zip(docs, doc_ids):
-        tokens = get_token_strings(doc)
-        types = set(token.lower() for token in tokens)
-        sentences = get_sentences(doc)
-
-        document = Document(
-            doc_id,
-            tokens,
-            types,
-            sentences,
-            doc,
-        )
-        all_documents.append(document)
+        all_documents.append(Document.from_spacy_document(doc, doc_id))
 
     return all_documents
---
+++ src/langviz/processing/load_spacy.py
@@ -0,0 +1,68 @@
+"""
+This module contains code for creating the spaCy NLP object with custom extentions
+"""
+
+from typing import Callable, Iterator, Set
+
+import pandas as pd
+import spacy
+from spacy.cli.download import download
+from spacy.language import Language
+from spacy.tokens import Doc
+
+### GETTERS
+
+
+def _get_tokens(doc: Doc) -> Iterator[str]:
+    for token in doc:
+        yield token.text
+
+
+def _get_types(doc: Doc) -> Set[str]:
+    return set(token.text for token in doc)
+
+
+def _get_entities_df(doc: Doc) -> pd.DataFrame:
+    return pd.DataFrame({"text": ent.text, "label": ent.label_} for ent in doc.ents)
+
+
+# EXTENSIONS
+
+CUSTOM_EXTENSIONS = {
+    ("tokens", _get_tokens),
+    ("types", _get_types),
+    ("entities_df", _get_entities_df),
+}
+
+
+def _set_spacy_extension(name: str, function: Callable) -> None:
+    """
+    Turns a given function into a spaCy extenstion accessed via a given name.
+    Updates the global state of the spaCy Doc object
+    """
+    if not Doc.has_extension(name):
+        Doc.set_extension(name, getter=function)
+
+
+def _apply_custom_extensions() -> None:
+    """Adds new extentions to the spaCy Doc object"""
+
+    for name, function in CUSTOM_EXTENSIONS:
+        _set_spacy_extension(name, function)
+
+
+# TODO: add support for custom models
+def load_spacy_model(model: str) -> Language:
+    """
+    Attempts to load given spaCy model and apply custom extentions.
+    Will try to download model if possible and not found on system
+    """
+    try:
+        nlp = spacy.load(model)
+    except OSError:
+        print(f"spaCy model '{model}' not detected. Downloading...")
+        download(model)
+        nlp = spacy.load(model)
+    nlp.max_length = 10000000
+    _apply_custom_extensions()
+    return nlp
